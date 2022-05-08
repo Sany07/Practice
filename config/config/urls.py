b6b88c1b876a092197abc76d379e1810721ace8d
Ahasan Habib Sany <sany.baust@gmail.com>
2022-05-08 12:02:27 +0600

    updated

--- config/config/urls.py
+++ config/config/urls.py
@@ -1,7 +1,7 @@
 """config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,13 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.decorators.cache import never_cache
+from django.views.static import serve
 from .views import *
-
+from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',Home),
-    path('gg/',Data),
+    path('', Home, name='home'),
+    path('data/', Data, name='data'),
+    path('product/', include('inventory.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, view=never_cache(serve))
